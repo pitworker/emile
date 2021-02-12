e0ef63a35596126ff2fadffa74b5053ab193327c
lassyla <a.alyssa.lee@gmail.com>
2021-02-12 13:18:05 -0500

    added unity project

---
+++ UnityProject/Emile/Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//procedural terrain starter code from Brackeys https://www.youtube.com/watch?v=64NblGkAabk
+[RequireComponent(typeof(MeshFilter))]
+public class GenerateTerrain : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    private int width = 10;
+    private int depth = 10; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh(); 
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[width * depth];
+        for (int w = 0; w < width; w++){
+            for(int d = 0; d < depth; d++)
+            {
+                vertices[w * depth + d] = new Vector3(w, 0, d);
+            }
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals(); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Emile/Assets/Scripts/GrassSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+using UnityEngine.VFX;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class GrassSlider : MonoBehaviour
+{
+    public VisualEffect grassEffect;
+    public Slider mainSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        mainSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        Debug.Log(mainSlider.value);
+        grassEffect.SetFloat("density", mainSlider.value);
+
+    }
+}
